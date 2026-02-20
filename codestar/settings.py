6c34d528671a1b848eafdec02c7b854feff383b8
TeasDale <jordanjohnteasdale@gmail.com>
2026-02-20 13:10:38 +0000

    created post class in models/blog

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a23i(2xie&46&yua05^73tfe!2&d!)h9^g4bza_&l1b)mni@#1'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com',
                  '127.0.0.1',]
