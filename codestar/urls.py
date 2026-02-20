14ddcb2ed59e0f8c17aed20c0f64be40c7d1f131
TeasDale <jordanjohnteasdale@gmail.com>
2026-02-20 12:09:52 +0000

    adopted code to match source code provided

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog import views
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', views.blog_home),
+    path('blog/', blog_views.my_blog, name='blog'),
 ]
