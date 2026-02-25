ce20a29c6f9a1520c5d258f81e027fee19646295
TeasDale <jordanjohnteasdale@gmail.com>
2026-02-25 11:02:52 +0000

    updated some spelling to previous commit that prevented deployment

--- about/models.py
+++ about/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class About(models.Model):
-    title = models.Charfield(max_length=200)
+    title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
