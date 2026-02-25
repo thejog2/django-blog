0154c6278474b1819d6a7555e7bf018480b85906
TeasDale <jordanjohnteasdale@gmail.com>
2026-02-25 13:04:41 +0000

    added form to about

--- about/views.py
+++ about/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
 
 
 def about_me(request):
@@ -10,9 +8,12 @@ def about_me(request):
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about, 
+         "collaborate_form": collaborate_form
+         },
     )
